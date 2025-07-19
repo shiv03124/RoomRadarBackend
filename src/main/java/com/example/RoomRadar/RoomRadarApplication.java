386399f8629b02a6596f81bb80e7ad5fd3887670
SHIVKUMARPATIL312 <shivkumarpatil.pjsofttech@gmail.com>
2025-07-19 12:02:43 +0530

    Remove application.properties from version control

--- src/main/java/com/example/RoomRadar/RoomRadarApplication.java
+++ src/main/java/com/example/RoomRadar/RoomRadarApplication.java
@@ -1,5 +1,6 @@
 package com.example.RoomRadar;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,20 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class RoomRadarApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+
+		// Set system properties from .env variables
+		System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
+		System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
+
+		System.setProperty("EMAIL_USERNAME", dotenv.get("EMAIL_USERNAME"));
+		System.setProperty("EMAIL_PASSWORD", dotenv.get("EMAIL_PASSWORD"));
+
+		System.setProperty("JWT_SECRET", dotenv.get("JWT_SECRET"));
+
+		System.setProperty("GOOGLE_CLIENT_ID", dotenv.get("GOOGLE_CLIENT_ID"));
+		System.setProperty("GOOGLE_CLIENT_SECRET", dotenv.get("GOOGLE_CLIENT_SECRET"));
+
 		SpringApplication.run(RoomRadarApplication.class, args);
 	}
 
