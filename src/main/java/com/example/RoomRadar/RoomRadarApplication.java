013539fd5189efa8e80ce165faa1bb4170dccaa6
SHIVKUMARPATIL312 <shivkumarpatil.pjsofttech@gmail.com>
2025-07-21 12:03:07 +0530

    Initial commit

--- src/main/java/com/example/RoomRadar/RoomRadarApplication.java
+++ src/main/java/com/example/RoomRadar/RoomRadarApplication.java
@@ -1,6 +1,5 @@
 package com.example.RoomRadar;
 
-import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,19 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class RoomRadarApplication {
 
 	public static void main(String[] args) {
-		Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
-
-		// Set system properties from .env variables
-		System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
-		System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
-
-		System.setProperty("EMAIL_USERNAME", dotenv.get("EMAIL_USERNAME"));
-		System.setProperty("EMAIL_PASSWORD", dotenv.get("EMAIL_PASSWORD"));
-
-		System.setProperty("JWT_SECRET", dotenv.get("JWT_SECRET"));
-
-
 		SpringApplication.run(RoomRadarApplication.class, args);
 	}
-
 }
